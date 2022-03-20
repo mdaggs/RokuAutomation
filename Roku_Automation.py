f7d753b70b6a499b02b126b14a91c7f5e518f916
mdlumen <matthew.daggs@lumen.com>
2022-03-20 11:21:14 -0500

    Finished for 1 TV

--- Roku_Automation.py
+++ Roku_Automation.py
@@ -1,46 +1,49 @@
-from gettext import install
 from roku import Roku
 from datetime import datetime
 from Timer import intervalTimer
 import time
 from config import hellenZaas
 
+#----- Define Your Roku Instance -----
 roku = Roku(hellenZaas)
 
+#----- Print Available Commands -----
 for value in roku.commands:
     print(value)
 
 print('\n')
 
+#----- Print Apps on Your Roku Device -----
 for value in roku.apps:
     print(value)
 
+#----- State Your App instances -----
 NewsOn6 = roku[580178]
-# NewsOn6.launch()
-# time.sleep(20)
-# roku.home()
 
-current_date = datetime.now().strftime("%H:%M:%S")
-print(current_date)
+#----- State on and off time logic -----
 startTime = '06:00:00'
 offTime = '09:00:00'
+
+
+#----- Print Current Intraday Date -----
+current_date = datetime.now().strftime("%H:%M:%S")
+print(current_date)
 
 # roku.home()
 while True:
     currentTime = datetime.now()
+    current_date = datetime.now().strftime("%H:%M:%S")
     dayOfWeek = currentTime.strftime("%a")
-    intradayTime = currentTime.strftime("%H:%M:%S")
 
-    if dayOfWeek != 'Sat' or dayOfWeek != "Sun" and current_date > startTime and current_date > offTime:
-        print(True)
-        print(dayOfWeek)
-        # NewsOn6.launch()
-        # time.sleep(10800)
-        # roku.home()
-        # time.sleep(20)
-        # roku.poweroff()
+    if dayOfWeek != 'Sat' or dayOfWeek != "Sun" and current_date > startTime and current_date < offTime:
+        print("Activate: True")
+        NewsOn6.launch()
+        time.sleep(10800)
+        roku.home()
+        time.sleep(20)
+        roku.poweroff()
 
     else:
-        print(False)
+        print("Activate: False")
 
     intervalTimer(1)
